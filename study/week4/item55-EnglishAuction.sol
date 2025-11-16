938f204b2e4f9a039bc514af9fb64c24c50c5d6b
jaylee3440 <jay6623905@qq.com>
2025-11-16 23:44:09 +0800

    test

---
+++ study/week4/item55-EnglishAuction.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract EnglishAuction{
+
+    IERC721 immutable public nft;
+    uint256 immutable public nftId;
+    uint256 immutable public startDate;
+    uint256 immutable public endDate;
+    uint256 immutable public startPrice;
+    uint256 immutable public x;
+
+
+}
